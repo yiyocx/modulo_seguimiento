c05d1cde58be8a9efc1bc650458761ccce63b1ee
Gustavo Lopez <gustavo725596@gmail.com>
2014-10-05 13:47:00 -0500

    Configuracion inicial de la gema devise

--- implementacion/config/routes.rb
+++ implementacion/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :informes
 
   resources :becarios do
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   resources :proyectos
 
   resources :visita_tecnicas
+
+  root 'becarios#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
