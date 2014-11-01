47767fec19162c47a8d1e62369c7a22b9c326705
xyiyox <yiyocx@gmail.com>
2014-11-01 14:49:01 -0500

    Agregada gema simplecov para inspeccion de codigo

--- implementacion/test/test_helper.rb
+++ implementacion/test/test_helper.rb
@@ -1,3 +1,16 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_group "Controllers", "app/controllers"
+  add_group "Helpers", "app/helpers"
+  add_group "Mailers", "app/mailers"
+  add_group "Lib", "lib"
+  add_group "Models", "app/models"  
+  add_group "Long files" do |src_file|
+    src_file.lines.count > 100
+  end
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
